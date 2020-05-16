3a508e38bbad8436ecfea0db0adfb390a171e82f
Sieun Sim <tlatldms@naver.com>
2020-05-17 05:29:05 +0900

    fixed path in addResourceHandlers

--- backend/src/main/java/com/lastpang/server/Config/WebConfig.java
+++ backend/src/main/java/com/lastpang/server/Config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.lastpang.server.Config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${static.resource.location}")
+    private String menuImgLocation;
+
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/menuimgs/**")
-                .addResourceLocations("classpath:/menuimgs/");
+                .addResourceLocations(menuImgLocation);
     }
 
     @Override
